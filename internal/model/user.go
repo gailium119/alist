5508318d5cb8f92d251b0b143e7c6f3a5227c6e7
gailium119 <77530553+gailium119@users.noreply.github.com>
2025-02-02 20:11:10 +0800

    Update user.go

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,11 +6,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/alist-org/alist/v3/internal/errs"
 	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/alist-org/alist/v3/pkg/utils/random"
 	"github.com/go-webauthn/webauthn/webauthn"
-	"github.com/pkg/errors"
 )
 
 const (
